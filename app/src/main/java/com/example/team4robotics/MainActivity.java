118f6d582175b42e93a4ac5262299d4683633514
kaanuzel <kaanuzel@sabanciuniv.edu>
2021-05-17 00:56:08 +0900

    new activities are created

--- app/src/main/java/com/example/team4robotics/MainActivity.java
+++ app/src/main/java/com/example/team4robotics/MainActivity.java
@@ -2,13 +2,51 @@ package com.example.team4robotics;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.security.PrivateKey;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonE;
+    private Button buttonK;
+    private Button buttonF;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonE = (Button) findViewById(R.id.buttonEnglish);
+        buttonK = (Button) findViewById(R.id.buttonKorean);
+        buttonF = (Button) findViewById(R.id.buttonFrench);
+
+        buttonE.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openAllFloorsActivity();
+            }
+        });
+        buttonK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openAllFloorsActivity();
+            }
+        });
+        buttonF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openAllFloorsActivity();
+            }
+        });
+    }
+    public void openAllFloorsActivity() {
+
+        Intent intent = new Intent(this, AllFloorsActivity.class);
+        startActivity(intent);
     }
 }
