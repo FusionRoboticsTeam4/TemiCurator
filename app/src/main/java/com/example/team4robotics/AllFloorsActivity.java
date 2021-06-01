e21872fc944d4e09b9eb329b0c40d8e3dceb0de9
kaanuzel <kaanuzel@sabanciuniv.edu>
2021-06-01 22:12:39 +0900

    retreive from firebase

--- app/src/main/java/com/example/team4robotics/AllFloorsActivity.java
+++ app/src/main/java/com/example/team4robotics/AllFloorsActivity.java
@@ -1,25 +1,32 @@
 package com.example.team4robotics;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class AllFloorsActivity extends AppCompatActivity {
 
-    private Button button1R;
-    private Button button2R;
-
+    TextView crowd;
+    String status;
+    DatabaseReference dref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_floors);
 
-        button1R = (Button) findViewById(R.id.buttonFirstRoom);
-
+        Button button1R = (Button) findViewById(R.id.buttonFirstRoom);
         button1R.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -28,7 +35,7 @@ public class AllFloorsActivity extends AppCompatActivity {
             }
         });
 
-        button2R = (Button) findViewById(R.id.buttonSecondRoom);
+        Button button2R = (Button) findViewById(R.id.buttonSecondRoom);
 
         button2R.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,6 +43,23 @@ public class AllFloorsActivity extends AppCompatActivity {
 
                 openSecondRoomActivity();
             }
+        });
+
+        crowd = (TextView) findViewById(R.id.roomCrowd);
+        dref = FirebaseDatabase.getInstance().getReference();
+        // Read from the database
+        dref.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot){
+                status = dataSnapshot.child("crowd").getValue().toString();
+                crowd.setText(status);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError){
+                
+            }
+
         });
     }
 
