b893361c4faf20bd3842fca10093ebf52897bfee
kaanuzel <kaanuzel@sabanciuniv.edu>
2021-06-22 13:54:27 +0900

    firebase

--- app/src/main/java/com/example/team4robotics/AllFloorsActivity.java
+++ app/src/main/java/com/example/team4robotics/AllFloorsActivity.java
@@ -1,5 +1,6 @@
 package com.example.team4robotics;
 
+import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,6 +21,9 @@ public class AllFloorsActivity extends AppCompatActivity {
     TextView crowd;
     String status;
     DatabaseReference dref;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,13 +50,14 @@ public class AllFloorsActivity extends AppCompatActivity {
         });
 
         crowd = (TextView) findViewById(R.id.roomCrowd);
-        dref = FirebaseDatabase.getInstance().getReference();
+        /*dref = FirebaseDatabase.getInstance().getReference("Crowds/Room1");
+
         // Read from the database
         dref.addValueEventListener(new ValueEventListener() {
 
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot){
-                status = dataSnapshot.child("crowd").getValue().toString();
+                status = dataSnapshot.getValue().toString();
                 crowd.setText(status);
             }
             @Override
@@ -60,6 +65,20 @@ public class AllFloorsActivity extends AppCompatActivity {
                 
             }
 
+        });*/
+        database.getReference("Crowds/Room1").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Object mytext = snapshot.getValue();
+                crowd.setText("Crowd:" + mytext.toString());
+                Log.d("tag", "crowd is " + mytext);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.d("tag", "Failed",error.toException());
+            }
         });
     }
 
@@ -71,4 +90,21 @@ public class AllFloorsActivity extends AppCompatActivity {
         Intent intent = new Intent(this, SecondRoomActivity.class);
         startActivity(intent);
     }
+
+
+    /*myRef.addValueEventListener(new ValueEventListener() {
+        @Override
+        public void onDataChange(DataSnapshot dataSnapshot) {
+            // This method is called once with the initial value and again
+            // whenever data at this location is updated.
+            String value = dataSnapshot.getValue(String.class);
+            Log.d(TAG, "Value is: " + value);
+        }
+
+        @Override
+        public void onCancelled(DatabaseError error) {
+            // Failed to read value
+            Log.w(TAG, "Failed to read value.", error.toException());
+        }
+    });*/
 }
